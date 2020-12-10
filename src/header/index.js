84503876dff4692d1edf715fa9b5bf72bc15be70
ResendiK2 <gabriel.arantesresende@gmail.com>
2020-12-09 22:37:04 -0300

    header styles

--- src/header/index.js
+++ src/header/index.js
@@ -4,7 +4,12 @@
 import styles from './styles.module.css';
 
 import { FaTimes } from "@react-icons/all-files/fa/FaTimes";
+import { FaTimesCircle } from "@react-icons/all-files/fa/FaTimesCircle";
 import { FaSearch } from "@react-icons/all-files/fa/FaSearch";
+import { FaRegHeart } from "@react-icons/all-files/fa/FaRegHeart";
+import { FaHome } from "@react-icons/all-files/fa/FaHome";
+import { FaRegPaperPlane } from "@react-icons/all-files/fa/FaRegPaperPlane";
+import { FaRegCompass } from "@react-icons/all-files/fa/FaRegCompass";
 
 function Header() {
     function logout() {
@@ -14,13 +19,30 @@ function Header() {
     return (
         <header className={styles.header}>
             <div className={styles.container}>
-                <div className={styles.background}>
-                    <img src="https://media-exp1.licdn.com/dms/image/C4E03AQHl4Nw0ESXmAA/profile-displayphoto-shrink_800_800/0?e=1612396800&v=beta&t=oCRHXfEjNLrIDz2BZBvDEREmcv3gjjPAdar02VgdprA" className={styles.image} />
+
+                <div className={styles.logoContainer}>
+                    <img
+                        src={"https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1200px-Instagram_logo.svg.png"}
+                        className={styles.logo}
+                    />
                 </div>
 
                 <div className={styles.search}>
                     <FaSearch size="15" />
                     <input className={styles.input} type={'text'} placeholder={"Pesquisar"} />
+                    <button className={styles.searchButton}>
+                        <FaTimesCircle size="15" color="rgb(78, 77, 77)" />
+                    </button>
+                </div>
+
+                <div className={styles.icons}>
+                    <FaHome size={23} />
+                    <FaRegPaperPlane size={23} />
+                    <FaRegCompass size={23} />
+                    <FaRegHeart size={23} />
+                    <div className={styles.background}>
+                        <img src="https://media-exp1.licdn.com/dms/image/C4E03AQHl4Nw0ESXmAA/profile-displayphoto-shrink_800_800/0?e=1612396800&v=beta&t=oCRHXfEjNLrIDz2BZBvDEREmcv3gjjPAdar02VgdprA" className={styles.image} />
+                    </div>
                 </div>
 
                 <div className={styles.buttonLogout}>
